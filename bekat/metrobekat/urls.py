88927f030719648355a32b146e2eea63df6285a7
Administrator <humoyunmahmudjinov@gmail.com>
2025-03-28 00:03:36 +0500

    view crudlar

--- bekat/metrobekat/urls.py
+++ bekat/metrobekat/urls.py
@@ -2,8 +2,26 @@ from django.urls import path
 from .views import *
 urlpatterns =[
     path('', index, name='home'),
-    path('list/', MetroList.as_view(), name='list'),
-    path('create/', MetroCreate.as_view(), name='create'),
-    path('delete/<int:pk>/', MetroDelete.as_view(), name='delete'),
-    path('delete/<int:pk>/', MetroUpdate.as_view(), name='update'),
+    path('listview/', MetroBekatView.as_view(), name='listview'),
+    path('createview/', MetroCreateView.as_view(), name='createview'),
+    path('updateview/<int:pk>/', MetroUpdateView.as_view(), name='updateview'),
+    path('deleteview/<int:pk>/', MetroBekatDeleteView.as_view(), name='deleteview'),
+    path('detailview/<int:pk>/', MetroBekatDetailView.as_view(), name='detailview'),
+    
+    # path('list/', MetroList.as_view(), name='list'),
+    # path('create/', MetroCreate.as_view(), name='create'),
+    # path('delete/<int:pk>/', MetroDelete.as_view(), name='delete'),
+    # path('delete/<int:pk>/', MetroUpdate.as_view(), name='update'),
+    # path('listcreate/', MetroListCreate.as_view(), name='listcreate'),
+    # path('retrievedelete/<int:pk>/', MetroRetrieveDestroy.as_view(), name='destroyretrieve'),
 ]
+
+
+###  viewset'ning url kodi, tepadagi kodni kommentga olib buni ishlatiladi
+
+# from .views import MetroViewSet
+# from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+# router.register(r'bekats', MetroViewSet, basename='bekat')
+# urlpatterns = router.urls
--- bekat/metrobekat/views.py
+++ bekat/metrobekat/views.py
@@ -2,25 +2,161 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializer import MetroBekatSerializer
 from .models import MetroBekat
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
 
 def index(r):
     return render(r, template_name='index.html', context={'metro':MetroBekat.objects.all()})
 
-class MetroList(generics.ListAPIView):
-    queryset = MetroBekat.objects.all()
-    serializer_class = MetroBekatSerializer
 
-class MetroCreate(generics.CreateAPIView):
-    queryset = MetroBekat.objects.all()
-    serializer_class = MetroBekatSerializer
+class MetroBekatView(APIView):
+    def get(self, r):
+        metro = MetroBekat.objects.all()
+        serializer = MetroBekatSerializer(metro, many=True)
+        response = {
+            'data':serializer.data,
+            'status': status.HTTP_200_OK,
+            'message': 'Metro List'
+        }
+        return Response(response)
 
-class MetroDelete(generics.DestroyAPIView):
-    queryset = MetroBekat.objects.all()
-    serializer_class = MetroBekatSerializer
-    lookup_field = 'pk'
+class MetroCreateView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = MetroBekatSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            rpns = {
+                'data': serializer.data,
+                'status': status.HTTP_201_CREATED,
+                "message": 'Bekat Created'
+            }
+            return Response(rpns)
+        else:
+            rpns = {
+                'data': serializer.errors,
+                'status': status.HTTP_400_BAD_REQUEST,
+                "message": 'Invalid Data'
+            }
+            return Response(rpns)
 
-class MetroUpdate(generics.UpdateAPIView):
-    queryset = MetroBekat.objects.all()
-    serializer_class = MetroBekatSerializer
-    lookup_field = 'pk'
+class MetroUpdateView(APIView):
+    def put(self, request, pk, *args, **kwargs):
+        book = MetroBekat.objects.get(pk=pk)
+        serializer = MetroBekatSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            rpns = {
+                'data': serializer.data,
+                'status': status.HTTP_200_OK,
+                "message": 'Book Updated'
+            }
+            
+        else:
+            rpns = {
+                'data': serializer.errors,
+                'status': status.HTTP_400_BAD_REQUEST,
+                "message": 'Invalid Data'
+            }
+        return Response(rpns)
+    
+    def patch(self, request, pk, *args, **kwargs):
+        book = MetroBekat.objects.get(pk=pk)
+        serializer = MetroBekatSerializer(book, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            rpns = {
+                'data': serializer.data,
+                'status': status.HTTP_200_OK,
+                "message": 'Book Updated'
+            }
+            
+        else:
+            rpns = {
+                'data': serializer.errors,
+                'status': status.HTTP_400_BAD_REQUEST,
+                "message": 'Invalid Data'
+            }
+        return Response(rpns)
+
+class MetroBekatDetailView(APIView):
+    def get(self, r, pk):
+        try:
+            metro = MetroBekat.objects.get(pk=pk)
+            serializer = MetroBekatSerializer(metro, many=True)
+            rspns = {
+                'data':serializer.data,
+                'status': status.HTTP_200_OK,
+                'message': 'Metro List'
+            }
+        except Exception as e:
+            rspns = {
+                'data': str(e),
+                'status': status.HTTP_404_NOT_FOUND,
+                'message': 'Not Found'
+            }
+        finally:
+            return Response(rspns)
+
+class MetroBekatDeleteView(APIView):
+    def delete(self, r, pk):
+        metro = MetroBekat.objects.get(pk=pk)
+        metro.delete()
+        rspns = {
+                'data': None,
+                'status': status.HTTP_204_NOT_CONTENT,
+                'message': 'Ochirildi'
+            }
+        return Response(rspns)
+        
+
+### vazifani 1-qismi
+### viewset crud kodi, ishlatilganda url qismidagi kommentga etibor qrating
+
+# class MetroViewSet(viewsets.ModelViewSet):
+#     queryset = MetroBekat.objects.all()
+#     serializer_class = MetroBekatSerializer
+#     lookup_field = 'pk'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class MetroList(generics.ListAPIView):
+#     queryset = MetroBekat.objects.all()
+#     serializer_class = MetroBekatSerializer
+
+# class MetroCreate(generics.CreateAPIView):
+#     queryset = MetroBekat.objects.all()
+#     serializer_class = MetroBekatSerializer
+
+# class MetroDelete(generics.DestroyAPIView):
+#     queryset = MetroBekat.objects.all()
+#     serializer_class = MetroBekatSerializer
+#     lookup_field = 'pk'
+
+# class MetroUpdate(generics.UpdateAPIView):
+#     queryset = MetroBekat.objects.all()
+#     serializer_class = MetroBekatSerializer
+#     lookup_field = 'pk'
+
+# class MetroListCreate(generics.ListCreateAPIView):
+#     queryset = MetroBekat.objects.all()
+#     serializer_class = MetroBekatSerializer
+
+# class MetroRetrieveDestroy(generics.RetrieveDestroyAPIView):
+#     queryset = MetroBekat.objects.all()
+#     serializer_class = MetroBekatSerializer
+#     lookup_field = 'pk'
+
+    
